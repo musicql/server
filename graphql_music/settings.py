75e3ab62c73e8efac5268b5cde221b281e1aa1a5
ahmad_kmal <ahmad.kmal@webteb.net>
2021-12-28 01:40:45 +0200

    heroku

--- graphql_music/settings.py
+++ graphql_music/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 GRAPHENE = {
     "SCHEMA": "graphql_music.schema.schema"
 }
+django_heroku.settings(locals())
